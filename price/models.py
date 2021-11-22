ddd30e8c9389e66c21bdb95c82d485c3b5b49472
JaeKwan <worhks37@likelion.org>
2021-11-22 13:28:36 +0900

    artwork artist upload to 추가

--- price/models.py
+++ price/models.py
@@ -3,14 +3,18 @@ from django.contrib.auth.models import User
 
 class Artist(models.Model):
     name = models.CharField(max_length=32)
-    image = models.ImageField()
+    image = models.ImageField(
+        upload_to="artist/image/%Y/%m/%d/%H"
+    )
     def __str__(self):
         return self.name
 
 class ArtWork(models.Model):
     title = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="artworks")
-    art_image = models.ImageField()
+    art_image = models.ImageField(
+        upload_to="artwork/image/%Y/%m/%d/%H"
+    )
     price = models.IntegerField()
     trade_date = models.DateField()
     material = models.CharField(max_length=32)
