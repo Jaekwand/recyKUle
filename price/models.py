90ca3df815eb0da463769b0585fdee96979c3a2c
devxlnt <devxlnt@gmail.com>
2021-11-22 15:24:06 +0900

    model revise

--- price/models.py
+++ price/models.py
@@ -1,24 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Artist(models.Model):
     name = models.CharField(max_length=32)
     image = models.ImageField(
-        upload_to="artist/image/%Y/%m/%d/%H"
+        upload_to="artist/image/%Y/%m/%d/%H", null=True, blank=True)
+    image_url = models.URLField(
+        default='',
+        blank=True
     )
+
     def __str__(self):
         return self.name
+
 
 class ArtWork(models.Model):
     title = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name="artworks")
     art_image = models.ImageField(
-        upload_to="artwork/image/%Y/%m/%d/%H"
+        upload_to="artwork/image/%Y/%m/%d/%H",
+        null=True,
+        blank=True
+    )
+    art_image_url = models.URLField(
+        default='',
+        blank=True
     )
     price = models.IntegerField()
     trade_date = models.DateField()
     material = models.CharField(max_length=32)
     size_width = models.CharField(max_length=32)
     size_height = models.CharField(max_length=32)
+
     def __str__(self):
         return self.title
